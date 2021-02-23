7b08bb41baa3c3c4742b278c01216135daa01b8e
hema <hema1.balusu1994@gmail.com>
2021-02-22 23:14:17 -0800

    s3mainonlysss

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "64TIPQQA2s0H+4axQZGrWMcLKYOhW87r9rlJl/Ba"
 }
 
-resource "aws_s3_bucket" "my-s301-bucket" {
+resource "aws_s3_bucket" "my-s301-13234454656775654-bucket" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "my-s301-bucket" {
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket" "my-s302-bucket" {
+resource "aws_s3_bucket" "my-s302-876249842-bucket" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "my-s302-bucket" {
     Environment = "Dev"
   }
 }
-resource "aws_s3_bucket" "my-s303-bucket" {
+resource "aws_s3_bucket" "my-s303-55631158990897-bucket" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
 
