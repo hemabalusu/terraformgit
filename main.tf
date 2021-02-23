e1e3578873185adc6959f17157deff7e1b0b7d38
hema <hema1.balusu1994@gmail.com>
2021-02-22 21:20:22 -0800

    s3main

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
