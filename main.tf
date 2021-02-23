6a651d8d8a6a5308fcd630b4b4f95e8415553496
hema <hema1.balusu1994@gmail.com>
2021-02-22 21:59:19 -0800

    s3mainonlys

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region     = "us-west-2"
-  my-access-key="AKIA2TYAN3PJR6DYXJWQ"
-my-secret-key="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"
+  access_key = "AKIA2TYAN3PJR6DYXJWQ"
+  secret_key = "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"
 }
+
 resource "aws_s3_bucket" "bucket1" {
   bucket = "my-tf-test-bucket"
   acl    = "private"
